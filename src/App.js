888209a2d11aa165166df8c067ce9cd5b1aea850
ryowuandjanet <ryowu0329@gmail.com>
2021-09-12 05:09:44 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-9xl text-center text-blue-800 uppercase sm:text-red-300 sm:bg-black">Ryowu</p>
+    </div>
+  );
+}
+
+export default App;
